78d492bb293f391cc9c5113d0b082dfe3de46b89
Edwin Miyatake <edmiyatake@gmail.com>
2026-08-24 13:21:04 -0700

    added SecureStore object
    changes the storage type in createClient from AsyncStorage to SecureStoreAdapter. Prevents people from seeing session tokens in travel

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,5 @@
 import 'react-native-url-polyfill/auto';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SecureStore from 'expo-secure-store';
 import { createClient } from '@supabase/supabase-js';
 
 // Set these in a .env file (see .env.example) and load them with a library
@@ -7,9 +7,15 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
+const SecureStoreAdapter = {
+  getItem: (key) => SecureStore.getItemAsync(key),
+  setItem: (key, value) => SecureStore.setItemAsync(key, value),
+  removeItem: (key) => SecureStore.deleteItemAsync(key),
+};
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
-    storage: AsyncStorage,
+    storage: SecureStoreAdapter,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
