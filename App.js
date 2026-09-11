a3f827ba7647e11f5fd559c549878e61de413aaa
Edwin Miyatake <edmiyatake@gmail.com>
2026-09-11 16:22:26 -0700

    Update App.js
    added a font import to app js because I was getting an error that the font could not be preloaded

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import * as SplashScreen from 'expo-splash-screen';
-import { useFonts, Fraunces_500Medium, Fraunces_500Medium_Italic } from '@expo-google-fonts/fraunces';
+import {
+  useFonts,
+  Fraunces_500Medium,
+  Fraunces_500Medium_Italic,
+  Fraunces_600SemiBold,
+} from '@expo-google-fonts/fraunces';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
 import { ThemeProvider, useTheme } from './src/theme/ThemeContext';
@@ -23,6 +28,7 @@ export default function App() {
   const [fontsLoaded, fontError] = useFonts({
     Fraunces_500Medium,
     Fraunces_500Medium_Italic,
+    Fraunces_600SemiBold,
     Inter_400Regular,
     Inter_500Medium,
   });
